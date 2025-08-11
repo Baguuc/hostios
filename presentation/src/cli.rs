e543ad1dbd1de4886d7c57ee4c9bf18d61e42807
Jan Żukowski <janzukowski@proton.me>
2025-08-11 15:49:29 +0200

    update: added route for reading directories

--- presentation/src/cli.rs
+++ presentation/src/cli.rs
@@ -58,6 +58,7 @@ async fn run(args: Args) {
             .service(crate::routes::tags::create::controller)
             .service(crate::routes::tags::delete::controller)
             .service(crate::routes::directories::create::controller)
+            .service(crate::routes::directories::read::controller)
     });
 
     let binded_server = match server.bind(("0.0.0.0", config.port.clone())) {
