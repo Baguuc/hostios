4a7d240d7af68e4dec2f069039064756635c2072
Jan Żukowski <janzukowski@proton.me>
2025-08-12 14:01:38 +0200

    util: changed Entry model's type to be renamed to lowercase when serialized

--- domain/src/models/entry.rs
+++ domain/src/models/entry.rs
@@ -1,5 +1,5 @@
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
-#[serde(tag = "type", content = "path")]
+#[serde(tag = "type", content = "path", rename_all = "lowercase")]
 pub enum Entry {
     Directory(crate::Path),
     File(crate::Path)
