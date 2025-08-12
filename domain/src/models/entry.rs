26410bbd897833fade050f2cd2fb1a48fbb0c3fe
Jan Żukowski <janzukowski@proton.me>
2025-08-12 13:57:13 +0200

    fix: fixed Entry model's serialization

--- domain/src/models/entry.rs
+++ domain/src/models/entry.rs
@@ -1,5 +1,5 @@
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
-#[serde(tag = "type")]
+#[serde(tag = "type", content = "path")]
 pub enum Entry {
     Directory(crate::Path),
     File(crate::Path)
